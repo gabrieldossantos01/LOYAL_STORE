c0678b72063cfa651aee4a4f1074a227297137d7
Gabriel dos santos silva <gabrieldossantossilvagb@gmail.com>
2025-01-21 15:07:58 -0300

    menu reset's

--- src/main/script.js
+++ src/main/script.js
@@ -1,1 +1,11 @@
- 
+ let click = window.document.getElementById('icon-burguer')
+ let nav = window.document.getElementById('nav-list')
+ click.addEventListener('click', menu)
+
+ function menu() {
+    if(nav.style.display == 'none'){
+        nav.style.display = 'block'} 
+        else{
+            nav.style.display = 'none'
+        }
+ } 
