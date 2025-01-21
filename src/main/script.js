17e48d461e76537e1d4345d7d7c3feaeea3208d0
Gabriel dos santos silva <gabrieldossantossilvagb@gmail.com>
2025-01-21 16:05:52 -0300

    dde

--- src/main/script.js
+++ src/main/script.js
@@ -4,7 +4,8 @@
 
  function menu() {
     if(nav.style.display == 'none'){
-        nav.style.display = 'block'} 
+        nav.style.display = 'block'
+    } 
         else{
             nav.style.display = 'none'
         }
