d419008df83606fa78bdf1789fca094fc2526d55
Gabriel dos santos silva <gabrieldossantossilvagb@gmail.com>
2025-05-09 12:53:48 -0300

    Update store to electronics theme

--- src/main/script.js
+++ src/main/script.js
@@ -1,12 +1,12 @@
- let click = window.document.getElementById('icon-burguer')
- let nav = window.document.getElementById('nav-list')
- click.addEventListener('click', menu)
+let click = window.document.getElementById('icon-burguer')
+let nav = window.document.getElementById('nav-list')
+click.addEventListener('click', menu)
 
- function menu() {
+function menu() {
+    click.classList.toggle('active');
     if(nav.style.display == 'none'){
         nav.style.display = 'block'
-    } 
-        else{
-            nav.style.display = 'none'
-        }
- } 
+    } else {
+        nav.style.display = 'none'
+    }
+}
